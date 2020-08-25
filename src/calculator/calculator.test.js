bc5d3c98c5499fd260805687a3896f40333bf055
Bertil Tandayamo <bertilt4@gmail.com>
2020-08-25 16:03:44 -0500

    Update test messages

--- src/calculator/calculator.test.js
+++ src/calculator/calculator.test.js
@@ -4,13 +4,16 @@ describe('calculator', () => {
   it('adds two numbers', () => {
     expect(calculator.add(2, 3)).toBe(5);
   });
-  it('substracts two numbers', () => {
+
+  it('subtracts two numbers', () => {
     expect(calculator.subtract(2, 3)).toBe(-1);
   });
+
   it('multiplies two numbers', () => {
     expect(calculator.multiply(2, 3)).toBe(6);
   });
-  it('throughs an error if the 2nd number is a negative or 0', () => {
-    expect(calculator.divide(10,0)).toEqual(Infinity);
+
+  it('returns an infinity error if the second number is a negative or 0', () => {
+    expect(calculator.divide(10, 0)).toEqual(Infinity);
   });
 });
