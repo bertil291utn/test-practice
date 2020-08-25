04184c3dd4a5d66d7bce9c5ed68e72d76c3a3f38
Bertil Tandayamo <bertilt4@gmail.com>
2020-08-25 12:46:51 -0500

    Take out overshift function - Take out of cipher main function

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -1,26 +1,23 @@
-const caesarCipher = (text, shift) => {
-  const overShift = (letter, shft) => {
-    const charVal = letter.charCodeAt() + shft;
-    if (charVal > 90 && letter.charCodeAt() < 90) {
-      return String.fromCharCode(charVal - 26);
-    }
-    if (charVal > 122) {
-      return String.fromCharCode(charVal - 26);
-    }
-    return String.fromCharCode(charVal);
-  };
+const overShift = (letter, shift) => {
+  const charVal = letter.charCodeAt() + shift;
+  if (charVal > 90 && letter.charCodeAt() < 90) {
+    return String.fromCharCode(charVal - 26);
+  }
+  if (charVal > 122) {
+    return String.fromCharCode(charVal - 26);
+  }
+  return String.fromCharCode(charVal);
+};
 
+const caesarCipher = (text, shift) => {
   const newText = [];
   text.split('').forEach((letter) => {
     if (letter.match(/[A-Za-z]/)) {
       newText.push(overShift(letter, shift));
-    }
-    else newText.push(letter);
+    } else newText.push(letter);
   });
 
   return newText.join('');
 };
 
-console.log(caesarCipher('bertil', 1));
-
-// export default caesarCipher;
+export default caesarCipher;
