524ee9e07b4717213b337cc9fc79f2183b2a6984
NewIncome <alfredo_cardenas@live.com>
2020-08-25 12:21:46 -0500

    Add code for the caesarCipher method

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -1,2 +1,23 @@
-const caesarCipher = (text, shift) => {};
+const caesarCipher = (text, shift) => {
+  const overShift = (letter, shft) => {
+    if ((letter.charCodeAt() + shft) > 90) {
+      return String.fromCharCode((letter.charCodeAt() + shft) - 26);
+    }
+    if ((letter.charCodeAt() + shft) > 122) {
+      return String.fromCharCode((letter.charCodeAt() + shft) - 26);
+    }
+    return String.fromCharCode(letter.charCodeAt() + shft);
+  };
+
+  const newText = [];
+  text.split('').forEach((letter) => {
+    if (letter.match(/[A-Za-z]/)) {
+      newText.push(overShift(letter, shift));
+    }
+    else newText.push(letter);
+  });
+
+  return newText.join('');
+};
+
 export default caesarCipher;
