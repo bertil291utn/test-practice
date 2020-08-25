176a63a1b8b1662e256d390e394075fa0f5f7629
Bertil Tandayamo <bertilt4@gmail.com>
2020-08-25 16:08:35 -0500

    Add capitalize test method

---
+++ src/capitalize/capitalize.test.js
@@ -0,0 +1,11 @@
+import capitalize from './capitalize';
+
+describe('capitalize text', () => {
+  it('returns a capitalized first letter', () => {
+    expect(capitalize('hello world')).toBe('Hello world');
+  });
+  
+  it('returns a capitalized first letter', () => {
+    expect(capitalize('1 first place')).toBe('1 first place');
+  });
+});
