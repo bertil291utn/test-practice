1cb506f1cbe4516a5f8292d64672d2e254bb613f
Bertil Tandayamo <bertilt4@gmail.com>
2020-08-25 10:28:04 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const App = (() => {
+  const capitalize = (word) => {};
+
+  const reverseString = (word) => {};
+
+  const calculator = {
+    add: (firstNumber, secondNumber) => firstNumber + secondNumber,
+  };
+
+  const caesarCipher = (text, shift) => {};
+
+  const arrayAnalysis = () => {};
+  return { capitalize, reverseString, calculator, caesarCipher };
+})();
+
+export default App;
