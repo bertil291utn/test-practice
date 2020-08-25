d58a7147ba9823941721acb506b0dceb9f11ffa4
Bertil Tandayamo <bertilt4@gmail.com>
2020-08-25 12:59:48 -0500

    Add array analysis function

--- src/array-analysis.js
+++ src/array-analysis.js
@@ -1,2 +1,12 @@
-const arrayAnalysis = () => {};
+/* eslint-disable dot-notation */
+const arrayAnalysis = (array) => {
+  const response = {
+    average: array.reduce((x, y) => x + y) / array.length,
+    length: array.length,
+    max: Math.max(...array),
+    min: Math.min(...array),
+  };
+
+  return response;
+};
 export default arrayAnalysis;
